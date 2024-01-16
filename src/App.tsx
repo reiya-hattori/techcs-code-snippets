dfe9e98f250938c5b1ee7132f65811d272a1529d
reiya-hattori <reiya.hattori@geniee.co.jp>
2024-01-16 16:34:28 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, Flex } from '@chakra-ui/react';
+
+import Header from './components/Header';
+import GoogleFeatures from './components/GoogleFeatures';
+import SignInFlows from './components/SignInFlows';
+
+import { Flows } from './types/enums';
+import AuthorizationFeatures from './components/AuthorizationFeatures';
 
 function App() {
+  const [flow, setFlow] = useState<Flows | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex
+      minH="100vh"
+      bg="gray.50"
+      direction="column"
+      align="center"
+      py="10"
+      px="4"
+    >
+      <Header />
+
+      {!flow ? (
+        <GoogleFeatures setFlow={setFlow} />
+      ) : (
+        <Button
+          onClick={() => setFlow(null)}
+          mb="5"
+          variant="ghost"
+          colorScheme="red"
+          textTransform="uppercase"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Restart
+        </Button>
+      )}
+      {flow === Flows.SignIn && <SignInFlows />}
+      {flow === Flows.Authorization && <AuthorizationFeatures />}
+    </Flex>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GoogleOAuthProvider clientId="529979054654-o6s2hatkivk24mb9otuvg2m2431hn8dk.apps.googleusercontent.com">
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
